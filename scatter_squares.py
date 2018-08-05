86e0d3b87d1b4f7b1db82840fbfd91430edf61d4
Faiz Khan <faizk6797@gmail.com>
2018-08-05 18:00:57 +0530

    Initial Plots and Visualizations

---
+++ scatter_squares.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+
+x_values=list(range(1,1001))
+y_values=[x**2 for x in x_values]
+plt.scatter(x_values,y_values,c=y_values,cmap=plt.cm.Blues,edgecolor='none',s=20)
+
+#set chart title and label title
+plt.title("Squared Numbers",fontsize=24)
+plt.xlabel("Value",fontsize=14)
+plt.ylabel("Square of Value",fontsize=14)
+
+#set size of tick labels
+plt.tick_params(axis='both',labelsize=14)
+
+#set the range for each axis
+plt.axis([0,1100,0,1100000])
+#plt.show()
+plt.savefig('squares_plot.png')
